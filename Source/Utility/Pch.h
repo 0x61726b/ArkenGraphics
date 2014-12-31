38239582a097240ec1e862521978579160f21784
arkenthera <arkengraphics@gmail.com>
2014-12-31 15:03:44 +0200

    Project Structure 2

---
+++ Source/Utility/Pch.h
@@ -0,0 +1,47 @@
+//--------------------------------------------------------------------------------
+//This is a file from Arkengine
+//
+//
+//Copyright (c) Alperen Gezer.All rights reserved.
+//
+//Pch.h
+//--------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------
+#ifndef __Pch_h__
+#define __Pch_h__
+//--------------------------------------------------------------------------------
+
+//Standart C++ Libraries
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+//
+
+//Standard C Library includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <memory>
+#include <assert.h>
+//
+
+//Direct3D Includes
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+
+#include <wrl.h>
+
+
+#define COM_Release( x ) {if(x){(x)->Release();(x)=NULL;}}
+#define COM_Delete( x ) {if(x){delete (x);(x)=NULL;}}
+
+
+//--------------------------------------------------------------------------------
+#endif //__Pch_h__
+
+
+
